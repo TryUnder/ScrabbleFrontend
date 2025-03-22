b3c7f47eac350ba73330faf96c342836c0d97ee7
TryUnder <sniacyinaczej@gmail.com>
2025-03-22 14:58:31 +0100

    Starting frontend project add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/test")  // Pobiera JSON z backendu
+      .then((response) => setData(response.text()))
+      .catch((error) => console.error("Błąd:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Test połączenia React ↔ .NET</h1>
+      {data ? (
+        <div>
+          <p><strong>Message:</strong> {data}</p>
+        </div>
+      ) : (
+        <p>Ładowanie...</p>
+      )}
     </div>
   );
 }
